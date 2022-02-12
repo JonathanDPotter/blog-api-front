99d156cfa85833d6ea67964e6cad612bed33a99e
JonathanDPotter <jonathan.d.potter@outlook.com>
2022-02-12 06:19:25 -0500

    change BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { debounce } from "debounce";
 // utils
 import { saveState } from "./store/localStorage";
